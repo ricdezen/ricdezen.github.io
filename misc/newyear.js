0f112810fb7a5455c4d792ddabb746da61f8bc84
Riccardo De Zen <riccardodezen98@gmail.com>
2020-10-01 13:17:26 +0200

    Moved Spectrum viewer to separate class

--- misc/newyear.js
+++ misc/newyear.js
@@ -1,10 +1,6 @@
 // Viewport width and height for canvas size.
 const VW = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const VH = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-// Number of frequency bands for spectrometer.
-const BANDS = 512;
-// p5 instance.
-var p5lib = new p5();
 
 // Format String for new year's date.
 var newYearStr = "1 January YEAR";
@@ -17,87 +13,24 @@ var newYears = new Date(newYearStr.replace("YEAR", nextYear.toString()));
 
 // Remaining seconds.
 var remaining = 0;
-// Wether the song is currently being played.
-var playing = false;
-// Wether a song has been loaded.
-var songReady = false;
-// Song file to play.
-var song = null;
 // Timer for countdown update.
 var timer = null;
-// Canvas to draw spectrometer on.
-var canvas = null;
-// Fft analyzer.
-var fft = null;
 
-/**
- * Setting up p5 canvas. 255 color levels for RGB and alpha.
- * 0.8 FFT smoothing.
- */
-function setup() {
-    canvas = p5lib.createCanvas(VW, VH);
-    canvas.class("spectrum");
-    p5lib.colorMode(p5lib.RGB, 255, 255, 255, 255);
-    p5lib.stroke(255, 255, 255, 100);
-    fft = new p5.FFT(0.8, BANDS);
-}
-
-/**
- * On each frame draw the spectrogram of the song being played on the canvas.
- */
-function draw() {
-    p5lib.clear();
-    bandW = canvas.width / BANDS;
-    bandMaxH = canvas.height * 0.6;
-    p5lib.strokeWeight(bandW * 0.66);
-    leftOffset = bandW / 2;
-    if (playing) {
-        var spectrum = fft.analyze();
-        for (var i = 0; i < BANDS; i++) {
-            var amp = spectrum[i];
-            if (amp == 0)
-                continue;
-            var x = p5lib.map(i, 0, BANDS, 0, canvas.width);
-            var y = p5lib.map(amp, 0, 255, 0, bandMaxH);
-            p5lib.line(x + leftOffset, canvas.height, x + leftOffset, canvas.height - y);
-        }
-    }
-}
-
-/**
- * Start playing the currently selected song.
- * @param {int} offset The offset at which to play the song. 
- */
-function playSong(offset) {
-    if (song != null && !playing && songReady) {
-        song.play();
-        song.jump(offset);
-        playing = true;
-    }
-}
-
-/**
- * Stop playing the currently selected song.
- */
-function stopSong() {
-    if (song != null && playing) {
-        song.stop();
-        playing = false;
-    }
-}
+// Spectrum visualizer.
+var spectrum = new Spectrum(64);
 
 /**
  * Callback for song file selection.
  * @param {*} event The file selection event.
  */
 function loadSong(event) {
-    var file = event.target.files[0];
-    stopSong();
-    songReady = false;
-    song = p5lib.loadSound(URL.createObjectURL(file), function () {
-        document.getElementById("song-desc").innerHTML = file.name;
-        songReady = true;
-    });
+    let file = event.target.files[0];
+    spectrum.changeSource(
+        URL.createObjectURL(file),
+        function () {
+            document.getElementById("song-desc").innerHTML = file.name;
+        }
+    );
 }
 
 /**
@@ -106,7 +39,7 @@ function loadSong(event) {
  */
 function loadOffset(event) {
     beforeZero = document.getElementById("offset").value;
-    stopSong();
+    spectrum.stop();
 }
 
 /**
@@ -115,8 +48,8 @@ function loadOffset(event) {
  */
 function loadBackground(event) {
     if (event.target.files && event.target.files[0]) {
-        var file = event.target.files[0];
-        var url = URL.createObjectURL(file);
+        let file = event.target.files[0];
+        let url = URL.createObjectURL(file);
         document.getElementById("parallax-container").style.backgroundImage = "url(" + url + ")";
         document.getElementById("image-desc").innerHTML = file.name;
     }
@@ -127,13 +60,13 @@ function loadBackground(event) {
  */
 function updateTimer() {
     remaining = Math.floor((newYears - new Date()) / 1000);
-    var seconds = remaining;
-    var minutes = Math.floor(seconds / 60);
-    var hours = Math.floor(minutes / 60);
-    var days = Math.floor(hours / 24);
+    let seconds = remaining;
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
 
     if (seconds <= beforeZero)
-        playSong(beforeZero - seconds);
+        spectrum.play(beforeZero - seconds);
 
     seconds -= minutes * 60;
     minutes -= hours * 60;
---
+++ misc/spectrum.js
@@ -0,0 +1,124 @@
+/**
+ * This class requires the p5 and p5-sound libraries to be loaded before it.
+ */
+class Spectrum {
+
+    constructor(/*id,*/ bands = 64) {
+        // ! Switch this out for something else.
+        this.width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        this.height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+        // ! Specify interface and container.
+        this.p5lib = new p5(this.makeSeed());
+        this.bands = bands;
+
+        // 0 smoothing, we will handle it ourselves.
+        this.fft = new p5.FFT(0);
+
+        // Wether the source is playing.
+        this.playing = false;
+        // Wether the source is ready.
+        this.ready = false;
+        // Audio source.
+        this.source = null;
+    }
+
+    /**
+     * @returns Return an interface that binds `setup` and `draw`.
+     */
+    makeSeed() {
+        return (sketch) => {
+            sketch.setup = () => {
+                // New canvas.
+                this.canvas = sketch.createCanvas(this.width, this.height);
+
+                // Alpha for colors, setting lines to semi-transparent white.
+                sketch.colorMode(sketch.RGB, 255, 255, 255, 255);
+                sketch.stroke(255, 255, 255, 100);
+
+                // ! Get rid of this, find parent size some way.
+                this.canvas.class("spectrum");
+            };
+
+            sketch.draw = () => {
+                if (!this.playing)
+                    return;
+                sketch.clear();
+                let bandW = this.canvas.width / this.bands;
+                let bandMaxH = this.canvas.height * 0.6;
+                sketch.strokeWeight(bandW * 0.66);
+                let leftOffset = bandW / 2;
+                if (this.playing) {
+                    let spectrum = this.fft.analyze();
+                    for (let i = 0; i < this.bands; i++) {
+                        let amp = spectrum[i];
+                        if (amp == 0)
+                            continue;
+                        let x = sketch.map(i, 0, this.bands, 0, this.canvas.width);
+                        let y = sketch.map(amp, 0, 255, 0, bandMaxH);
+                        sketch.line(
+                            x + leftOffset,
+                            this.canvas.height,
+                            x + leftOffset,
+                            this.canvas.height - y
+                        );
+                    }
+                }
+            };
+        };
+    }
+
+    /**
+     * Stop the playback and set the audio source to `source`.
+     * @param source Any suitable audio source for `p5.loadSound`.
+     * @param callback Callback for when audio is ready for playback.
+     */
+    changeSource(source, callback) {
+        this.stop();
+        this.ready = false;
+        let caller = this;
+        this.source = this.p5lib.loadSound(
+            source,
+            function () {
+                // Using `this` here refers to `p5lib`.
+                caller.ready = true;
+                callback();
+            }
+        );
+    }
+
+    /**
+     * Play the current source starting at the specified offset in seconds.
+     * If already playing, this method does nothing.
+     * @param offset The offset in seconds at which to start playback.
+     */
+    play(offset) {
+        if (this.source == null || offset > this.source.duration())
+            return;
+        if (this.playing || !this.ready)
+            return;
+        // Play, go to offset.
+        this.source.play();
+        this.source.jump(offset);
+        this.playing = true;
+    }
+
+    /**
+     * Stop the source being currently played. Does nothing if not playing or
+     * the source is null.
+     */
+    stop() {
+        if (this.source == null || !this.playing)
+            return;
+        this.source.stop();
+        this.playing = false;
+    }
+
+    /**
+     * @returns Wether the audio is being played or not.
+     */
+    isPlaying() {
+        return this.playing;
+    }
+
+};
