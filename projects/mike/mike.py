0e043aa777d5a2cceb7ccb419a56767300e5f88e
ricdezen <riccardodezen98@gmail.com>
2022-05-20 15:51:22 +0200

    Article about Pathfinder point buy page

--- projects/mike/mike.py
+++ projects/mike/mike.py
@@ -1,5 +1,4 @@
 from browser import document, timer
-import itertools
 import random
 
 # POINTS[x] = how many points a score of x is worth.
@@ -34,29 +33,34 @@ def eval_alloc(alloc: list) -> int:
     return sum(POINTS[i + 7] * x for i, x in enumerate(alloc))
 
 
-def enumerate_sub(depth: int, already: int):
-    # Already have 6 stats.
+def enumerate_rec(depth: int, already: int) -> list:
+    """
+    :param depth: How many values are left to set.
+    :param already: Sum of previous values.
+                    Equivalently, how many stats have been set.
+    """
+    # Already have 6 stats. The last `depth` values are 0.
     if already > 5:
-        # print(f"More than 5 stats at depth: {depth}")
         return [[0] * depth]
-    # Last value in the sequence (12 possible values).
+
+    # Last value in the sequence. Return the only value that makes 6.
     if depth == 1:
-        # print(f"Bottom, already={already}: {[[x] for x in range(7-already)]}")
-        return [[x] for x in range(7-already)]
-    # Enumerate remaining stats.
+        return [[6 - already]]
+
+    # Enumerate remaining feasible values.
     result = []
-    for how_many in range(7 - already):
-        sub_enum = enumerate_sub(depth - 1, how_many + already)
-        for s in sub_enum:
-            result.append([how_many]+s)
-    # print(f"depth={depth}, already={already}: {result}")
+    for n in range(7 - already):
+        # Get all values compatible with this value being `n`.
+        for s in enumerate_rec(depth - 1, n + already):
+            result.append([n] + s)
+
     return result
 
 
 def enumerate_stats() -> list:
     # Returns a list of all possible stats allocations (unique)
     # So, how many 7, 8, 9 scores etc.
-    return [x for x in enumerate_sub(12, 0) if sum(x) == 6]
+    return enumerate_rec(12, 0)
 
 
 def main():
